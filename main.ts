24a14fa9f1d8e872b2a5b85d796872c4fe4207bf
Lemonexus <67396147+Lemonexus@users.noreply.github.com>
2020-11-21 13:26:10 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,44 @@ namespace SpriteKind {
     export const Grocery = SpriteKind.create()
     export const CartItem = SpriteKind.create()
 }
-let groceryImages = [
+
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Grocery, function(p: Sprite, g: Sprite) {
+    if (controller.A.isPressed()){
+        addToCart(g)
+        pause(100)
+    }
+})
+
+function addToCart(grocery: Sprite){
+    let item = sprites.create(grocery.image, SpriteKind.CartItem)
+    item.x = player.x
+    item.y = player.y
+    item.follow(player)
+}
+
+function createProduct (productImg: Image, cost: number, weight: number, name: string) {
+    p = sprites.create(productImg, SpriteKind.Grocery)
+    sprites.setDataNumber(p, "cost", cost)
+    sprites.setDataNumber(p, "weight", weight)
+    sprites.setDataString(p, "name", name)
+    tiles.placeOnRandomTile(p, myTiles.tile1)
+}
+
+function createAllProducts () {
+    for (let i = 0; i < groceryImages.length; i++) {
+        createProduct(groceryImages[i], groceryCosts[i], groceryWeights[i], groceryNames[i])
+    }
+}
+let name = ""
+let weight = 0
+let cost = 0
+let image2: Image = null
+let p: Sprite = null
+let groceryCosts: number[] = []
+let groceryWeights: number[] = []
+let groceryNames: string[] = []
+let groceryImages: Image[] = []
+groceryImages = [
 img`
     . . . 2 2 2 . . . . . . . . . . 
     . . . c c c 6 6 8 8 . . . . . . 
@@ -16,6 +53,24 @@ img`
     8 6 9 9 9 9 9 9 9 9 9 8 . . . . 
     . 8 6 9 9 9 9 9 9 9 6 8 . . . . 
     . . 8 8 8 8 8 8 8 8 8 . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `,
+img`
+    . . . 8 8 8 . . . . . . . . . . 
+    . . . f f f f f f f . . . . . . 
+    . . f e e e e e e f f . . . . . 
+    . f e e e e e e f e f f . . . . 
+    f e e e e e e f . f e f . . . . 
+    f e e e e e e f . f e f . . . . 
+    f e e e e e e f . f e f . . . . 
+    f e e e e e e f f e e f . . . . 
+    f e e e e e e e e e e f . . . . 
+    f f e e e e e e e e e f . . . . 
+    . f f e e e e e e e e f . . . . 
+    . . f f f f f f f f f . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -77,20 +132,20 @@ img`
     `,
 img`
     . . . . . . . . . . . . . . . . 
-    . . 6 6 9 9 9 9 . . . . . . . . 
-    . 6 9 9 1 1 1 1 9 . . . . . . . 
-    6 9 6 9 9 9 1 9 1 9 . . . . . . 
-    6 9 9 6 6 6 6 1 1 9 . . . . . . 
-    6 9 9 6 9 9 6 1 1 9 . . . . . . 
-    . 6 9 6 9 9 6 1 9 . . . . . . . 
-    . . 6 9 6 6 1 9 . . . . . . . . 
-    . . . 6 9 9 9 . . . . . . . . . 
-    . . . . 6 6 . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . f e e e e e e f . . . . 
+    . . . f e e e e e e e e f . . . 
+    . . . f e e f f f f f e f . . . 
+    . . . f e f d d d d d e e f . . 
+    . . f e e f d f d f d e e f . . 
+    . . f e e f d e d e d e e f . . 
+    . . f e e f d d d d f e e f . . 
+    . f e e f 1 f f f f 1 f e e f . 
+    . . f f 1 1 1 1 1 1 1 1 f f . . 
+    . . . f c 9 2 1 1 1 1 1 1 f . . 
+    . . . f 1 1 1 1 1 1 1 f f . . . 
+    . . . f f 1 1 1 1 1 1 1 f . . . 
+    . . . . f f f f f f f f f . . . 
     . . . . . . . . . . . . . . . . 
     `,
 img`
@@ -166,34 +221,37 @@ img`
     . . . . 6 6 6 6 6 6 6 . . . . . 
     `
 ]
-let groceryNames = [
+groceryNames = [
 "Milk",
+"Chocolate Milk",
 "Grape Soda",
 "Oatmeal",
 "Turkey",
-"Fancy glass",
+"Ashley",
 "Chicken soup",
 "Sardines",
 "Flour",
 "Watermelon"
 ]
-let groceryWeights = [
+groceryWeights = [
+8,
 8,
 2,
 1,
 12,
-0.5,
+13,
 0.5,
 0.5,
 5,
 10
 ]
-let groceryCosts = [
+groceryCosts = [
 2,
+3,
 3,
 4,
 20,
-10,
+50,
 2,
 1,
 5,
@@ -202,20 +260,24 @@ let groceryCosts = [
 scene.setBackgroundColor(9)
 tiles.setTilemap(tilemap`level`)
 let player = sprites.create(img`
-    fffffff......................
-    f.fffcd......................
-    ..ffddc......................
-    ..fdddf......................
-    ..fdddd......................
-    ...55........................
-    ..55dddbbbbbbbbbbbb..........
-    ..555....b..b..b..b..........
-    .5555....bbbbbbbbbb..........
-    .5555....b..b..b..b..........
-    .5555....bbbbbbbbbb..........
+    eeeeeee......................
+    eeeeedd......................
+    eeeeddc......................
+    eeedddf......................
+    eeedddd......................
+    eee11........................
+    ee11dddbbbbbbbbbbbb..........
+    ee821....b..b..b..b..........
+    e1111....bbbbbbbbbb..........
+    .1111....b..b..b..b..........
+    .1111....bbbbbbbbbb..........
     .6666.....b.b..b.bb..........
     .6.66......bbbbbbb...........
     .d.d.......d.................
     .d..d......ddddddd...........
     .d..dd......c....c...........
     `, SpriteKind.Player)
+controller.moveSprite(player)
+scene.cameraFollowSprite(player)
+tiles.placeOnTile(player, tiles.getTileLocation(1, 3))
+createAllProducts()
